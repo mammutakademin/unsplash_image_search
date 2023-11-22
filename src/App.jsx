8d12b7e0a65e844f307907949bb6b528fe8a0438
Editha Trinitaria <edithtrinitaria.gmail.com>
2023-11-22 02:35:24 +0100

    Add validation for entered search term and loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,37 @@ const App = () => {
   const [ images, setImages ] = useState([]);
   const [ totalPages, setTotalPages ] = useState(0);
   const [ page, setPage ] = useState(1);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [ loading, setLoading ] = useState(false);
   
   const fetchImages = useCallback(async () => {
     try {
-      const { data } = await axios.get(
-        `${API_URL}?query=${
-          searchInput.current.value
-        }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
-      console.log('data', data);
-      setImages(data.results);
-      setTotalPages(data.total_pages);
-    } catch (error) {
-      console.log(error);
+      if (searchInput.current.value) {
+        setErrorMsg('');
+        setLoading(true);
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        console.log('data', data);
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+        setLoading(false);
+      }   
+    }
+      catch (error) {
+        setErrorMsg('Error fetching images. Try again later.');
+        console.log(error);
+        setLoading(false);
     }
   }, [page]);
 
   useEffect(() => {
     fetchImages();
-  }, [fetchImages, page]);
+  }, [fetchImages]);
 
     const resetSearch = () => {
     setPage(1);
@@ -49,11 +59,10 @@ const App = () => {
     resetSearch();
   };
 
-  console.log('page', page);
-
   return (
     <div className="container">
       <h1 className="title">Image Search</h1>
+      {errorMsg && <p className='error-msg'>{errorMsg}</p>}
       <div className="search-section">
         <Form onSubmit={handleSearch}>
           <Form.Control
@@ -70,25 +79,32 @@ const App = () => {
         <div onClick={() => handleSelection('cats')}>Cats</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
-      <div className="images">
-        {images.map((image) => (
-          <img 
-            key={image.id}
-            src={image.urls.small}
-            alt={image.alt_description}
-            className='image'
-          />
-        ))}
-      </div>
-      <div className="buttons">
-          {page > 1 && (
-            <Button onClick={() => setPage(page - 1)}>Previous</Button>
-          )}
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <>
+          <div className="images">
+          {images.map((image) => (
+            <img 
+              key={image.id}
+              src={image.urls.small}
+              alt={image.alt_description}
+              className='image'
+            />
+          ))}
+          </div>
+          <div className="buttons">
+            {page > 1 && (
+              <Button onClick={() => setPage(page - 1)}>Previous</Button>
+            )}
           
-          {page < totalPages && (
-            <Button onClick={() => setPage(page + 1)}>Next</Button>
-          )}
-      </div>
+            {page < totalPages && (
+              <Button onClick={() => setPage(page + 1)}>Next</Button>
+            )}
+          </div>
+        </>
+      )}
+        
     </div>
   )
 };
